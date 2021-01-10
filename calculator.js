2da90d997ae73fdb0d1b0dc15153b0fefeaf69e2
hajarghannami <77221957+hajarghannami@users.noreply.github.com>
2021-01-10 15:40:00 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,20 @@
+let firstNum = prompt("Enter your first Number: ");
+let secondNum = prompt("Enter your second Number: "); 
 
+let userOper  = prompt("Enter an operation: "); 
+
+if (userOper === '+') {
+
+  console.log(firstNum + secondNum); 
+
+} else if (userOper === '-') {
+
+  console.log(firstNum - secondNum); 
+  
+} else if (userOper === '*') {
+
+  console.log(firstNum * secondNum); 
+} else if (userOper === '/') {
+
+  console.log(firstNum / secondNum); 
+}
